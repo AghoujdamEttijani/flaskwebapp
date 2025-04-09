981874cfb5eade948ae2a77b017d2bf61aad43ca
AghoujdamEttijani <73637427+AghoujdamEttijani@users.noreply.github.com>
2025-04-10 00:26:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from datetime import datetime, timedelta
 import os
 
 app = Flask(__name__)
-app.config['MONGO_URI'] = 'mongodb+srv://<cherifaswak>:<cherif2019>@renderproject1cluster.tc990wu.mongodb.net/milk_store'
+app.config['MONGO_URI'] = 'mongodb+srv://<cherifaswak>:<cherif2019>@renderproject1cluster.tc990wu.mongodb.net/'
 app.config['SECRET_KEY'] = '1a2b3c'
 app.config['UPLOAD_FOLDER'] = 'static/uploads'
 mongo = PyMongo(app)
