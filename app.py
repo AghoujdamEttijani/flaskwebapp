55f7f5f499a99881463abba03b2676ac6cf5ba04
AghoujdamEttijani <73637427+AghoujdamEttijani@users.noreply.github.com>
2025-04-12 20:01:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -15,22 +15,28 @@ app.config['SECRET_KEY'] = '1a2b3c'
 app.config['UPLOAD_FOLDER'] = 'static/uploads'
 mongo = PyMongo(app)
 
-@app.route('/')
+# Home Route
+@app.route('/home')
 def home():
     default_username = "khalfiabdelilah"
     default_password = "khalfi**aloe"
     default_role = "admin"
 
-    existing_admin = mongo.db.users.find_one({"username": default_username})
-    if not existing_admin:
-        hashed_password = generate_password_hash(default_password)
-        mongo.db.users.insert_one({
+    try:
+         existing_admin = mongo.db.users.find_one({"username": default_username})
+         if not existing_admin:
+            hashed_password = generate_password_hash(default_password)
+            mongo.db.users.insert_one({
             "username": default_username,
             "password": hashed_password,
             "role": default_role
         })
+    except Exception as e:
+        print("MongoDB query failed:", e)
+    existing_admin = None
+
     products = mongo.db.products.find()
-    return render_template('index.html', products=products)
+  
 # send_email 
 #Configure Flask-Mail with Gmail SMTP
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
