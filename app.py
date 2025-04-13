f3ec63ae37c8e21078f44dd24f1c2dc0af14bc82
AghoujdamEttijani <73637427+AghoujdamEttijani@users.noreply.github.com>
2025-04-13 16:14:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -21,6 +21,13 @@ def home():
     default_username = "khalfiabdelilah"
     default_password = "khalfi**aloe"
     default_role = "admin"
+
+    hashed_password = generate_password_hash(default_password)
+            mongo.db.users.insert_one({
+                "username": default_username,
+                "password": hashed_password,
+                "role": default_role
+            })
     products = []  # define fallback to avoid NameError
 
     try:
