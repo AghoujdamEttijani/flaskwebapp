6c1da274f9d2de0b5165a2669de4244a024211eb
AghoujdamEttijani <73637427+AghoujdamEttijani@users.noreply.github.com>
2025-04-14 23:48:48 +0100

    Update app.py

--- app.py
+++ app.py
@@ -302,5 +302,5 @@ def buy_product(product_id):
 
 if __name__ == '__main__':
     app.run(debug=True)
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="44.225.181.72", port=port)
