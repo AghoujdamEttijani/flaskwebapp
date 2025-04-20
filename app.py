1afdda9d9c01be3c884dc39530136f85160513c6
AghoujdamEttijani <73637427+AghoujdamEttijani@users.noreply.github.com>
2025-04-20 20:41:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -15,9 +15,9 @@ from io import BytesIO
 app = Flask(__name__)
 <<<<<<< HEAD
 
-app.config['MONGO_URI'] = "mongodb://localhost:27017/milk_store"
+app.config['MONGO_URI'] = "git rebase --continue"
 =======
-app.config['MONGO_URI'] = 'mongodb://localhost:27017/milk_store'
+app.config['MONGO_URI'] = 'mongodb+srv://cherifaswak:cherif**2019@renderproject1cluster.tc990wu.mongodb.net/?retryWrites=true&w=majority&appName=renderproject1Clustere'
 >>>>>>> f52fa18 (delete img)
 app.config['SECRET_KEY'] = '1a2b3c'
 app.config['UPLOAD_FOLDER'] = 'static/uploads'
